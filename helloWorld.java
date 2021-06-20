32695e4664f79d2a2b9918a3330c31506e23eac3
sm0l voicc <72088954+sm0lvoicc@users.noreply.github.com>
2021-06-20 23:56:51 +0530

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+public class helloWorld /*class name and the folder name should be the same*/
+{
+  public static void main(String[] args) //declaring a public function, the main one
+  {
+    System.out.println("Hello World");
+    /*
+    System is your system, so its outputting a printing of a line in a new line which will be Hello World
+    */
+  }
+}
